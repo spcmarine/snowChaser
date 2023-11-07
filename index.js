484ea5ff433f17ac3d281074abd653bf93c90452
Dan Gibson <spcmarine@btinternet.com>
2023-11-07 12:57:01 +0000

    api call made

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const apiKey = require('./api')
-const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}
+const resort = "Morzine";
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${resort}&appid=${apiKey}`;
+
+
+// let weatherData = []
+
+// const getData = () => {
+//     fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => weatherData.push(data))
+// }
+
+// getData();
+
+// console.log(weatherData)
+
+let weatherData = null;
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((weatherData) => {
+    console.log(weatherData.weather[0].main);
+  });
+
+console.log("Requesting weather data");
+
+
